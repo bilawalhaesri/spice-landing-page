3da88527c0e216a9c2a0c94bfd84e2cd48cac8c5
Bilawal Haesri <billawalhaesri@gmail.com>
2023-10-26 19:53:42 +0700

    Spice LP JS Scroll Navbar Add

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function navbarScroll(){
     let navbar = document.querySelector('header');
-    navbar.classList.remove('navbar');
-    navbar.classList.add('navbar-scroll');
-    setTimeout(function(){
-        navbar.classList.add('navbar');
+    if (window.scrollY>100){
+        navbar.classList.add('navbar-scroll');
+        navbar.classList.remove('navbar');
+        document.querySelector('.logo').style.color='#ffffff'
+        document.querySelector('.hamburger a').style.color='#ffffff'
+        for(i=0;i<document.querySelectorAll('.navbar-menu ul li a').length;i++){
+            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#FFFFFF';
+        }
+        for(i=0;i<document.querySelectorAll('.navbar-side ul li a').length;i++){
+            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#FFFFFF';
+        }
+    }
+    else{
         navbar.classList.remove('navbar-scroll');
-    },500);
+        navbar.classList.add('navbar');
+        document.querySelector('.logo').style.color='#D68861'
+        document.querySelector('.hamburger a').style.color='#D68861'
+        for(i=0;i<document.querySelectorAll('.navbar-menu ul li a').length;i++){
+            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#D68861';
+        }
+        for(i=0;i<document.querySelectorAll('.navbar-side ul li a').length;i++){
+            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#D68861';
+        }
+    }
 }
 
 document.addEventListener('scroll',function(){
