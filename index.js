0ff20d8c501d26a9b963e5d3663d11bb676ab643
Bilawal Haesri <billawalhaesri@gmail.com>
2023-10-26 23:43:30 +0700

    Color Update

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function navbarScroll(){
     if (window.scrollY>100){
         navbar.classList.add('navbar-scroll');
         navbar.classList.remove('navbar');
-        document.querySelector('.logo').style.color='#ffffff'
-        document.querySelector('.hamburger a').style.color='#ffffff'
+        document.querySelector('.logo').style.color='#F9F8F2'
+        document.querySelector('.hamburger a').style.color='#F9F8F2'
         for(i=0;i<document.querySelectorAll('.navbar-menu ul li a').length;i++){
-            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#FFFFFF';
+            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#F9F8F2';
         }
         for(i=0;i<document.querySelectorAll('.navbar-side ul li a').length;i++){
-            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#FFFFFF';
+            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#F9F8F2';
         }
     }
     else{
