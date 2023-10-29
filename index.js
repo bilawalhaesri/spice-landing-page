456161465963345d476f952e3f22659eae68fb47
Bilawal Haesri <billawalhaesri@gmail.com>
2023-10-29 16:35:14 +0700

    Spice Landing Page Done

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 function navbarScroll(){
-    let navbar = document.querySelector('header');
-    if (window.scrollY>100){
-        navbar.classList.add('navbar-scroll');
-        navbar.classList.remove('navbar');
-        document.querySelector('.logo').style.color='#F9F8F2'
-        document.querySelector('.hamburger a').style.color='#F9F8F2'
-        for(i=0;i<document.querySelectorAll('.navbar-menu ul li a').length;i++){
-            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#F9F8F2';
-        }
-        for(i=0;i<document.querySelectorAll('.navbar-side ul li a').length;i++){
-            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#F9F8F2';
-        }
+    if(window.scrollY>100){
+        $('header').addClass('navbar-scroll');
+        $('header').removeClass('navbar');
+        $('.logo').css('color','#F9F8F2');
+        $('.hamburger a').css('color','#F9F8F2');
+        $('.navbar-menu ul li a').css('color','#F9F8F2');
+        $('.navbar-side ul li a').css('color','#F9F8F2');
     }
     else{
-        navbar.classList.remove('navbar-scroll');
-        navbar.classList.add('navbar');
-        document.querySelector('.logo').style.color='#D68861'
-        document.querySelector('.hamburger a').style.color='#D68861'
-        for(i=0;i<document.querySelectorAll('.navbar-menu ul li a').length;i++){
-            document.querySelectorAll('.navbar-menu ul li a')[i].style.color = '#D68861';
-        }
-        for(i=0;i<document.querySelectorAll('.navbar-side ul li a').length;i++){
-            document.querySelectorAll('.navbar-side ul li a')[i].style.color = '#D68861';
-        }
+        $('header').removeClass('navbar-scroll');
+        $('header').addClass('navbar');
+        $('.logo').css('color','#D68861');
+        $('.hamburger a').css('color','#D68861');
+        $('.navbar-menu ul li a').css('color','#D68861');
+        $('.navbar-side ul li a').css('color','#D68861');
     }
 }
 
-document.addEventListener('scroll',function(){
+$(document).on('scroll',function(){
     navbarScroll();
-})
+});
